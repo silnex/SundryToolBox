b2f113e9e91d8aed4e57de1509eb963fe0bc8b2d
silnex <24711610+SilNex@users.noreply.github.com>
2020-06-01 16:12:05 +0900

    Update ubuntu-php-init.sh

--- ubuntu-php-init.sh
+++ ubuntu-php-init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get update && \
 sudo apt -y install software-properties-common && \
-sudo add-apt-repository ppa:ondrej/php && \
-sudo apt-get install php7.4-cli php7.4-bcmath php7.4-bz2 php7.4-common php7.4-curl php7.4-dev
+sudo add-apt-repository -y ppa:ondrej/php && \
+sudo apt-get -y install php7.4-cli php7.4-bcmath php7.4-bz2 php7.4-common php7.4-curl php7.4-dev
 php7.4-fpm php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-sqlite3 php7.4-xml php7.
 4-xsl php7.4-zip unzip zip && \
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && \

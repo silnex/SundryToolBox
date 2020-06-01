9943ca1d3aa1fff0f76cf5261856ef8ddadf10f7
silnex <24711610+SilNex@users.noreply.github.com>
2020-06-01 14:37:38 +0900

    Update ubuntu-php-init.sh

--- ubuntu-php-init.sh
+++ ubuntu-php-init.sh
@@ -7,7 +7,10 @@ php7.4-fpm php7.4-gd php7.4-intl php7.4-json php7.4-mbstring php7.4-mysql php7.4
 4-xsl php7.4-zip unzip zip && \
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && \
 sudo php composer-setup.php --install-dir=/usr/bin --filename=composer && \
-sudo rm composer-setup.php
+sudo rm composer-setup.php && \
+echo 'export PATH="$HOME/.composer/vendor/bin:$PATH"' >> ~/.bashrc && \
+echo "alias artisan='php artisan'" >> ~/.bashrc && \
+source ~/.bashrc
 # wget -c http://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb && \
 # sudo dpkg -i mysql-apt-config_0.8.15-1_all.deb && \
 # sudo rm mysql-apt-config_0.8.15-1_all.deb && \
